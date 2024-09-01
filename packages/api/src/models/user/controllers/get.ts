4fc1575ccdb18ee1132d92b7be4debd32fe4af8f
cazans <caique.campelo@ccc.ufcg.edu.br>
2024-09-01 18:57:15 -0300

    fit: config for tests and user crud

---
+++ packages/api/src/models/user/controllers/get.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+import { protectedProcedure } from "../../../trpc";
+
+export const getUsers = protectedProcedure.query(({ ctx }) => {
+  return ctx.db.user.findMany();
+});
+
+export const getUserById = protectedProcedure
+  .input(z.object({ id: z.string() }))
+  .query(({ ctx, input }) => {
+    return ctx.db.user.findFirst({
+      where: { id: input.id },
+    });
+  });
---
+++ packages/api/src/models/user/user.ts
@@ -0,0 +1,29 @@
+import { createTRPCRouter } from "../../trpc";
+import { createUser } from "./controllers/create";
+import { deleteUser } from "./controllers/delete";
+import { getUsers, getUserById } from "./controllers/get";
+import { updateUser } from "./controllers/update";
+
+const createRouter = createTRPCRouter({
+  one: createUser,
+});
+
+const getRouter = createTRPCRouter({
+  one: getUserById,
+  all: getUsers,
+});
+
+const deleteUserRouter = createTRPCRouter({
+  one: deleteUser,
+});
+
+const updateUserRouter = createTRPCRouter({
+  one: updateUser,
+});
+
+export const userRouter = createTRPCRouter({
+  create: createRouter,
+  update: updateUserRouter,
+  get: getRouter,
+  delete: deleteUserRouter,
+});
