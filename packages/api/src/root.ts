5e893ce5df6a0f66d227fa75b302dc1f5ae10f0b
Ian Evangelista Rodrigues <ian.rodrigues@ccc.ufcg.edu.br>
2024-09-03 21:31:59 -0300

    Add billRouter to root.ts file on router package

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,12 +1,14 @@
 import { groupRouter } from "./models/group/group";
 import { userRouter } from "./models/user/user";
 import { authRouter } from "./router/auth";
+import { billRouter } from "./models/bill/bill";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   user: userRouter,
   group: groupRouter,
+  bill: billRouter,
 });
 
 // export type definition of API
