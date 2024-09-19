c15f58720e9e6a71289e6ac06f320a907deae050
BrunoRRamos <suelensamarasf@gmail.com>
2024-09-18 21:20:32 -0300

    chore: adiciona ajustes

--- apps/expo/src/app/(tabs)/index.tsx
+++ apps/expo/src/app/(tabs)/index.tsx
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import { ScrollView, TouchableOpacity, View } from "react-native";
 import { Redirect } from "expo-router";
 import { useAuth, useUser } from "@clerk/clerk-expo";
-import { FontAwesome5 } from "@expo/vector-icons";
+import {
+  LucideArrowRight,
+  LucideBell,
+  LucideHouse,
+  LucideLogOut,
+  LucidePlus,
+  LucideSearch,
+} from "lucide-react-native";
 
 import { Button, Text } from "~/components/ui";
 
@@ -39,8 +46,8 @@ export default function Home() {
 
   return (
     <View className="flex-1 bg-white p-6">
-      <View className="mb-4 flex-row items-center justify-between">
-        <View className="mt-4 flex-row items-center">
+      <View className="mb-4 flex flex-row items-center justify-between">
+        <View className="flex flex-row items-center">
           <View className="h-10 w-10 items-center justify-center rounded-full bg-gray-300">
             <Text>{userInitials}</Text>
           </View>
@@ -51,9 +58,9 @@ export default function Home() {
             </Text>
           </View>
         </View>
-        <View className="mb-4 flex-row items-center justify-end">
+        <View className="flex flex-row items-center">
           <TouchableOpacity onPress={() => console.log("Notificações")}>
-            <FontAwesome5 name="bell" size={24} />
+            <LucideBell size={24} color={"#000"} />
           </TouchableOpacity>
           <Button
             variant="link"
@@ -64,47 +71,47 @@ export default function Home() {
             }}
             loading={loading}
           >
-            <FontAwesome5 name="sign-out-alt" size={24} color={"#950101"} />
+            <LucideLogOut size={24} color={"#950101"} />
           </Button>
         </View>
       </View>
 
-      <Text className="mb-4 mt-4 text-xl text-black">Pagamentos Pendentes</Text>
+      <Text className="mb-4 text-xl text-black">Pagamentos Pendentes</Text>
       <Text className="mb-9 text-2xl font-bold text-black">R$ 537,50</Text>
 
       <ScrollView className="flex-1">
         <View className="mb-4 rounded-md border border-gray-300 p-4">
-          <View className="flex-row items-center justify-between">
+          <View className="flex flex-row items-center justify-between">
             <Text className="text-lg text-black">Aniversário de Caique</Text>
             <Text className="text-sm text-gray-600">Aberto</Text>
           </View>
 
-          <View className="mt-2 flex-row items-center justify-between">
+          <View className="mt-2 flex flex-row items-center justify-between">
             <Text className="text-lg font-bold text-black">R$ 37,50</Text>
             <Text className="text-sm font-bold">até 27/02</Text>
           </View>
 
           <View className="mt-2 items-end">
             <TouchableOpacity onPress={() => console.log("Seta clicada")}>
-              <FontAwesome5 name="arrow-right" size={16} />
+              <LucideArrowRight size={20} color={"#000"} />
             </TouchableOpacity>
           </View>
         </View>
 
         <View className="mb-4 rounded-md border border-gray-300 p-4">
-          <View className="flex-row items-center justify-between">
+          <View className="flex flex-row items-center justify-between">
             <Text className="text-lg text-black">Contas de casa</Text>
             <Text className="text-sm text-gray-600">Aberto</Text>
           </View>
 
-          <View className="mt-2 flex-row items-center justify-between">
+          <View className="mt-2 flex flex-row items-center justify-between">
             <Text className="text-lg font-bold text-black">R$ 500,00</Text>
             <Text className="text-sm font-bold">até 27/02</Text>
           </View>
 
           <View className="mt-2 items-end">
             <TouchableOpacity onPress={() => console.log("Seta clicada")}>
-              <FontAwesome5 name="arrow-right" size={16} />
+              <LucideArrowRight size={20} color={"#000"} />
             </TouchableOpacity>
           </View>
         </View>
@@ -112,15 +119,15 @@ export default function Home() {
 
       <View className="mb-2 h-px bg-gray-300" />
 
-      <View className="flex-row justify-around py-4">
+      <View className="flex flex-row justify-around py-4">
         <TouchableOpacity onPress={() => console.log("Home")}>
-          <FontAwesome5 name="home" size={24} />
+          <LucideHouse size={24} color={"#000"} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => console.log("Add Payment")}>
-          <FontAwesome5 name="plus" size={24} />
+          <LucidePlus size={24} color={"#000"} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => console.log("Search")}>
-          <FontAwesome5 name="search" size={24} />
+          <LucideSearch size={24} color={"#000"} />
         </TouchableOpacity>
       </View>
     </View>
